7481c7fa725d1180f02e7db3b53cf7d8fa58bda5
Luciano Barcaro <luciano.barcaro@gmail.com>
2019-01-08 15:42:37 -0200

    dynamic locale on all funcions

--- wowpy/__init__.py
+++ wowpy/__init__.py
@@ -22,10 +22,10 @@ class WowAPI(object):
         self._locale = locale
         self._multi_size = 50
 
-    def _iget(self, url: str, data: dict=None) -> dict:
+    def _iget(self, url: str, data: dict=None, locale: str=None) -> dict:
         """Internal use only"""
         dados = {
-            'locale': self._locale,
+            'locale': locale or self._locale,
             'apiKey': self._api_key
         }
         if isinstance(data, dict):
@@ -39,7 +39,7 @@ class WowAPI(object):
         return req.json()
 
     # Character services ----------------------
-    def char_info(self, realm: str, charname: str, details: list=None) -> dict:
+    def char_info(self, realm: str, charname: str, details: list=None, locale: str=None) -> dict:
         """Get info from specific char.
   *** Input parameters:
     * realm: Realm of the char...
@@ -58,7 +58,7 @@ class WowAPI(object):
         else:
             data = None
 
-        return self._iget('/character/{}/{}'.format(realm, charname), data)
+        return self._iget('/character/{}/{}'.format(realm, charname), data, locale)
 
     def multi(self, fnc: str, param_list: tuple) -> list:
         """Get info from specific char.
@@ -90,13 +90,13 @@ class WowAPI(object):
         return result
 
     # Game services
-    def game_achievement_info(self, achievement_id):
+    def game_achievement_info(self, achievement_id, locale: str=None):
         assert isinstance(achievement_id, int)
 
-        return self._iget('/achievement/{}'.format(achievement_id))
+        return self._iget('/achievement/{}'.format(achievement_id), locale)
 
-    def game_auction_data(self, realm, lastModified=0):
-        resp = self._iget('/auction/data/{realm}'.format(realm=realm))
+    def game_auction_data(self, realm, lastModified=0, locale: str=None):
+        resp = self._iget('/auction/data/{realm}'.format(realm=realm), locale=locale)
         retorno = []
         for file in resp['files']:
             if file['lastModified'] > lastModified:
@@ -104,99 +104,99 @@ class WowAPI(object):
 
         return retorno
 
-    def game_boss_list(self):
-        return self._iget('/boss/')
+    def game_boss_list(self, locale: str=None):
+        return self._iget('/boss/', locale=locale)
 
-    def game_boss_info(self, boss_id):
+    def game_boss_info(self, boss_id, locale: str=None):
         assert isinstance(boss_id, int)
 
-        return self._iget('/boss/{}'.format(boss_id))
+        return self._iget('/boss/{}'.format(boss_id), locale=locale)
 
-    def game_challenge_realm_leaderboard(self, realm):
-        return self._iget('/challenge/{}'.format(realm))
+    def game_challenge_realm_leaderboard(self, realm, locale: str=None):
+        return self._iget('/challenge/{}'.format(realm), locale=locale)
 
-    def game_challenge_region_leaderboard(self):
-        return self._iget('/challenge/region')
+    def game_challenge_region_leaderboard(self, locale: str=None):
+        return self._iget('/challenge/region', locale=locale)
 
-    def game_item_info(self, item_id):
+    def game_item_info(self, item_id, locale: str=None):
         assert isinstance(item_id, int)
 
-        return self._iget('/item/{}'.format(item_id))
+        return self._iget('/item/{}'.format(item_id), locale=locale)
 
-    def game_item_set(self, itemset_id):
+    def game_item_set(self, itemset_id, locale: str=None):
         assert isinstance(itemset_id, int)
 
-        return self._iget('/item/set/{}'.format(itemset_id))
+        return self._iget('/item/set/{}'.format(itemset_id), locale=locale)
 
-    def game_mount_list(self):
-        return self._iget('/mount/')
+    def game_mount_list(self, locale: str=None):
+        return self._iget('/mount/', locale=locale)
 
-    def game_pvp_leaderboards(self, bracket='rbg'):
+    def game_pvp_leaderboards(self, bracket='rbg', locale: str=None):
         if bracket not in ('2v2', '3v3', '5v5', 'rbg'):
             raise WowAPIError('Invalid pvp bracket')
 
-        return self._iget('/leaderboard/{}'.format(bracket))
+        return self._iget('/leaderboard/{}'.format(bracket), locale=locale)
 
-    def game_quest_info(self, quest_id):
+    def game_quest_info(self, quest_id, locale: str=None):
         assert isinstance(quest_id, int)
 
-        return self._iget('/quest/{}'.format(quest_id))
+        return self._iget('/quest/{}'.format(quest_id), locale=locale)
 
-    def game_realm_status(self, realms=None):
+    def game_realm_status(self, realms=None, locale: str=None):
         data = {'realms': ','.join(realms)} if realms else None
-        return self._iget('/realm/status', data)
+        return self._iget('/realm/status', data, locale=locale)
 
-    def game_recipe_info(self, recipe_id):
+    def game_recipe_info(self, recipe_id, locale: str=None):
         assert isinstance(recipe_id, int)
 
-        return self._iget('/recipe/{}'.format(recipe_id))
+        return self._iget('/recipe/{}'.format(recipe_id), locale=locale)
 
-    def game_spell_info(self, spell_id):
+    def game_spell_info(self, spell_id, locale: str=None):
         assert isinstance(spell_id, int)
 
-        return self._iget('/spell/{}'.format(spell_id))
+        return self._iget('/spell/{}'.format(spell_id), locale=locale)
 
-    def game_zone_list(self):
-        return self._iget('/zone/')
+    def game_zone_list(self, locale: str=None):
+        return self._iget('/zone/', locale=locale)
 
-    def game_zone_info(self, zone_id):
+    def game_zone_info(self, zone_id, locale: str=None):
         assert isinstance(zone_id, int)
 
-        return self._iget('/zone/{}'.format(zone_id))
+        return self._iget('/zone/{}'.format(zone_id), locale=locale)
 
-    def game_battlegroups_list(self):
-        return self._iget('/data/battlegroups/')
+    def game_battlegroups_list(self, locale: str=None):
+        return self._iget('/data/battlegroups/', locale=locale)
 
-    def game_races_list(self):
-        return self._iget('/data/character/races')
+    def game_races_list(self, locale: str=None):
+        return self._iget('/data/character/races', locale=locale)
 
-    def game_classes_list(self):
-        return self._iget('/data/character/classes')
+    def game_classes_list(self, locale: str=None):
+        return self._iget('/data/character/classes', locale=locale)
 
-    def game_achievements_list(self):
-        return self._iget('/data/character/achievements')
+    def game_achievements_list(self, locale: str=None):
+        return self._iget('/data/character/achievements', locale=locale)
 
-    def game_itemclass_list(self):
-        return self._iget('/data/item/classes')
+    def game_itemclass_list(self, locale: str=None):
+        return self._iget('/data/item/classes', locale=locale)
 
-    def game_talents_list(self):
-        return self._iget('/data/talents')
+    def game_talents_list(self, locale: str=None):
+        return self._iget('/data/talents', locale=locale)
 
     # pet services
-    def pet_list(self):
-        return self._iget('/pet/')
+    def pet_list(self, locale: str=None):
+        return self._iget('/pet/', locale=locale)
 
-    def pet_ability(self, ability_id):
+    def pet_ability(self, ability_id, locale: str=None):
         assert isinstance(ability_id, int)
 
-        return self._iget('/pet/ability/{}'.format(ability_id))
+        return self._iget('/pet/ability/{}'.format(ability_id), locale=locale)
 
-    def pet_species(self, species_id):
+    def pet_species(self, species_id, locale: str=None):
         assert isinstance(species_id, int)
 
-        return self._iget('/pet/species/{}'.format(species_id))
+        return self._iget('/pet/species/{}'.format(species_id), locale=locale)
 
-    def pet_stats(self, species_id, level=1, breed_id=3, quality_id=1):
+    def pet_stats(self, species_id, level=1, breed_id=3, quality_id=1, locale: str=None):
         assert isinstance(species_id, int) | isinstance(level, int) | isinstance(breed_id, int) | isinstance(quality_id, int)
 
         data = {
@@ -205,26 +205,26 @@ class WowAPI(object):
             'qualityId': quality_id
         }
 
-        return self._iget('/pet/stats/{}'.format(species_id), data)
+        return self._iget('/pet/stats/{}'.format(species_id), data, locale=locale)
 
-    def pet_types_list(self):
-        return self._iget('/data/pet/types')
+    def pet_types_list(self, locale: str=None):
+        return self._iget('/data/pet/types', locale=locale)
 
     # Guild services
-    def guild_info(self, realm, guild_name, details=None):
+    def guild_info(self, realm, guild_name, details=None, locale: str=None):
         valid_details = ('members', 'achievements', 'news', 'challenge')
         if details:
             data = {'fields': ','.join([detail for detail in details if detail in valid_details])}
         else:
             data = None
 
-        return self._iget('/guild/{}/{}'.format(realm, guild_name), data)
+        return self._iget('/guild/{}/{}'.format(realm, guild_name), data, locale=locale)
 
-    def guild_rewards_list(self):
-        return self._iget('/data/guild/rewards')
+    def guild_rewards_list(self, locale: str=None):
+        return self._iget('/data/guild/rewards', locale=locale)
 
-    def guild_perks_list(self):
-        return self._iget('/data/guild/perks')
+    def guild_perks_list(self, locale: str=None):
+        return self._iget('/data/guild/perks', locale=locale)
 
-    def guild_achievements_list(self):
-        return self._iget('/data/guild/achievements')
+    def guild_achievements_list(self, locale: str=None):
+        return self._iget('/data/guild/achievements', locale=locale)
